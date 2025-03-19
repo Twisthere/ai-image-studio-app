013da57fbced5de8c221cada3f6be83d6d56ad2e
Manthan Ankolekar <manthan.ank46@gmail.com>
2025-03-19 19:06:20 +0530

    Add health check route and improve server startup log

--- backend/server.js
+++ backend/server.js
@@ -9,5 +9,10 @@ app.use(cors());
 
 connectDB();
 app.use("/api/image", imageRoutes);
+app.use("/", (req, res) => {
+  res.send("API is running...");
+});
 
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on port ${process.env.PORT}`)
+);
